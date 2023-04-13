8bfa6c66b14f44c1a02e62217707ad4a3bfc7873
Gianni Schiralli <gschiralli@myseneca.ca>
2023-04-13 16:30:42 -0400

    added reusable button component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,9 @@
+import Button from "@/components/ui/Button";
+
 export default function Home() {
-  return <div className="text-red-500">Hello World</div>;
+  return (
+    <div className="">
+      <Button>Send</Button>
+    </div>
+  );
 }
