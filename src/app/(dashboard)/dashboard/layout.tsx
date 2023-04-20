06c2fa8c4a8c3cef37756f42a6184c81b916ecf9
Gianni Schiralli <gschiralli@myseneca.ca>
2023-04-20 09:54:17 -0400

    added add friend sidebar option

--- src/app/(dashboard)/dashboard/layout.tsx
+++ src/app/(dashboard)/dashboard/layout.tsx
@@ -3,10 +3,25 @@ import { getServerSession } from "next-auth";
 import Link from "next/link";
 import { notFound } from "next/navigation";
 import { FC, ReactNode } from "react";
+import { Icon, Icons } from "./../../../components/icons";
 
 interface LayoutProps {
   children: ReactNode;
 }
+interface SidebarOption {
+  id: number;
+  name: string;
+  href: string;
+  Icon: Icon;
+}
+const sidebarOptions: SidebarOption[] = [
+  {
+    id: 1,
+    name: "Add Friend",
+    href: "/dashboard/add",
+    Icon: "UserPlus",
+  },
+];
 
 const Layout = async ({ children }: LayoutProps) => {
   const session = await getServerSession(authOptions);
@@ -15,10 +30,41 @@ const Layout = async ({ children }: LayoutProps) => {
   return (
     <div className="w-full flex h-screen">
       <div className="flex h-full w-full max-w-xs grow flex-col gap-y-5 overflow-y-auto border-r border-gray-200 bg-white px-6">
-        <Link
-          href="/dashboard"
-          className="flex h-16 shrink-0 items-center"
-        ></Link>
+        <Link href="/dashboard" className="flex h-16 shrink-0 items-center">
+          <Icons.Logo className="h-8 w-auto text-indigo-600" />
+        </Link>
+        <div className="text-xs font-semibold leading-6 text-gray-400">
+          your chats
+        </div>
+
+        <nav className="flex flex-1 flex-col">
+          <ul role="list" className="flex flex-1 flex-col gap-y-7">
+            <li>Chats that user has</li>
+            <li>
+              <div className="text-xs font-semibold leading-6 text-gray-400">
+                Overview
+              </div>
+              <ul role="list" className="-mx-2 mt-2 space-y-1">
+                {sidebarOptions.map((option) => {
+                  const Icon = Icons[option.Icon];
+                  return (
+                    <li key={option.id}>
+                      <Link
+                        href={option.href}
+                        className="text-gray-700 hover:text-indigo-600 hover:bg-gray-50 group flex gap-3 rounded-md p-2 text-sm leading-6 font-semibold"
+                      >
+                        <span className="text-gray-400 border-gray-200 group-hover:border-indigo-600 group-hover:text-indigo-600 flex h-6 w-6 shrink-0 items-center justify-center rounded-lg border text-[0.625rem] font-medium bg-white">
+                          <Icon className="h-4 w-4" />
+                        </span>
+                        <span className="truncate">{option.name}</span>
+                      </Link>
+                    </li>
+                  );
+                })}
+              </ul>
+            </li>
+          </ul>
+        </nav>
       </div>
       {children}
     </div>
