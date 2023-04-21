40e56d616c8f7091904ba86888b78fa3fa0ef762
Gianni Schiralli <gschiralli@myseneca.ca>
2023-04-21 13:44:50 -0400

    added accept and deny api routes

--- src/components/FriendRequests.tsx
+++ src/components/FriendRequests.tsx
@@ -19,7 +19,7 @@ const FriendRequests: FC<FriendRequestsProps> = ({
   );
 
   const acceptFriend = async (senderId: string) => {
-    await axios.post("/api/requests/accept", { id: senderId });
+    await axios.post("/api/friends/accept", { id: senderId });
 
     setFriendRequests((prev) =>
       prev.filter((request) => request.senderId !== senderId)
@@ -27,7 +27,7 @@ const FriendRequests: FC<FriendRequestsProps> = ({
     router.refresh();
   };
   const denyFriend = async (senderId: string) => {
-    await axios.post("/api/requests/deny", { id: senderId });
+    await axios.post("/api/friends/deny", { id: senderId });
 
     setFriendRequests((prev) =>
       prev.filter((request) => request.senderId !== senderId)
@@ -44,6 +44,7 @@ const FriendRequests: FC<FriendRequestsProps> = ({
             <UserPlus className="text-black" />
             <p className="font-medium text-lg">{request.senderEmail}</p>
             <button
+              onClick={() => acceptFriend(request.senderId)}
               aria-label="accept friend"
               className="w-8 h-8 bg-indigo-600 hover:bg-indigo-700 grid place-items-center rounded-full transition hover:shadow-md"
             >
@@ -51,6 +52,7 @@ const FriendRequests: FC<FriendRequestsProps> = ({
             </button>
 
             <button
+              onClick={() => denyFriend(request.senderId)}
               aria-label="Deny friend"
               className="w-8 h-8 bg-red-600 hover:bg-red-700 grid place-items-center rounded-full transition hover:shadow-md"
             >
