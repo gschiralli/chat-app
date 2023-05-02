c8e888a05155e517d1b102c524794ce1d1f44fe8
Gianni Schiralli <gschiralli@hotmail.com>
2023-05-02 16:26:20 -0400

    Added messages component

--- src/app/(dashboard)/dashboard/chat/[chatId]/page.tsx
+++ src/app/(dashboard)/dashboard/chat/[chatId]/page.tsx
@@ -5,7 +5,8 @@ import { messageArrayValidator } from "@/lib/validations/message";
 import { getServerSession } from "next-auth";
 import { notFound } from "next/navigation";
 import { FC } from "react";
-
+import Image from "next/image";
+import Messages from "@/components/Messages";
 interface PageProps {
   params: {
     chatId: string;
@@ -48,7 +49,32 @@ const page = async ({ params }: PageProps) => {
   const chatPartner = (await db.get(`user:${chatPartnerId}`)) as User;
   const initialMessages = await getChatMessages(chatId);
   return (
-    <div className="flex-1 justify-between flex flex-col h-full max-h-[calc(100vh-6rem)]"></div>
+    <div className="flex-1 justify-between flex flex-col h-full max-h-[calc(100vh-6rem)]">
+      <div className="flex sm:items-center justify-between py-3 border-b-2 border-gray-200">
+        <div className="relative flex items-center space-x-4 ">
+          <div className="relative ">
+            <div className="relative w-8 sm:w-12 h-8 sm:h-12">
+              <Image
+                fill
+                referrerPolicy="no-referrer"
+                src={chatPartner.image}
+                alt={`${chatPartner.name} profile picture`}
+                className=" rounded-full"
+              />
+            </div>
+          </div>
+          <div className="flex flex-col leading-tight">
+            <div className="text-xl flex items-center">
+              <span className="text-gray-700 mr-3 font-semibold">
+                {chatPartner.name}
+              </span>
+            </div>
+            <span className="text-sm text-gray-600">{chatPartner.email}</span>
+          </div>
+        </div>
+      </div>
+      <Messages initialMessages={initialMessages} sessionId={session.user.id} />
+    </div>
   );
 };
 
