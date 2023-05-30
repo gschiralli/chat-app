ac0d3b0588338ed5485322f93d63215c9499bebd
Gianni Schiralli <gschiralli@myseneca.ca>
2023-05-30 10:50:02 -0400

    Updated dashboard

--- src/app/api/friends/accept/route.ts
+++ src/app/api/friends/accept/route.ts
@@ -1,6 +1,8 @@
 import { fetchRedis } from "@/helpers/redis";
 import { authOptions } from "@/lib/auth";
 import { db } from "@/lib/db";
+import { pusherServer } from "@/lib/pusher";
+import { toPusherKey } from "@/lib/utils";
 import { getServerSession } from "next-auth";
 import { z } from "zod";
 
@@ -37,12 +39,15 @@ export async function POST(req: Request) {
     if (!hasFriendRequest) {
       return new Response("No friend request", { status: 400 });
     }
+    pusherServer.trigger(
+      toPusherKey(`user:${idToAdd}:friends`),
+      "new-friend",
+      {}
+    );
 
     await db.sadd(`user:${session.user.id}:friends`, idToAdd);
 
     await db.sadd(`user:${idToAdd}:friends`, session.user.id);
-
-    // await db.srem(`user:${idToAdd}:incoming_friend_requests`, session.user.id);
 
     await db.srem(`user:${session.user.id}:incoming_friend_requests`, idToAdd);
     return new Response("OK");
